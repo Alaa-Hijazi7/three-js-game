b2f6b8bafc0ededc4774cc8a0af5e27898afa17f
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-11-10 14:10:24 +0200

    all

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { Canvas, useFrame, useThree } from "@react-three/fiber"
+import { Physics, useSphere, useBox, usePlane } from "@react-three/cannon"
+import "./App.css";
+
+function Ball({ args = [0.5, 32, 32] }) {
+  const { viewport } = useThree();
+  const [ref, api] = useSphere(() => ({ args: 0.5, mass: 1 }));
+  usePlane(() => ({
+    position: [0, -viewport.height, 0],
+    rotation: [-Math.PI / 2, 0, 0],
+    onCollide: () => {
+      api.position.set(0, 0, 0);
+      api.velocity.set(0, 5, 0);
+    },
+  }));
+  return (
+    <mesh ref={ref}>
+      <sphereGeometry args={args} />
+      <meshStandardMaterial />
+    </mesh>
+  )
+}
+
+function Paddle({ args = [5, 0.5, 1] }) {
+  const [ref, api] = useBox(() => ({ args }));
+  useFrame(state => {
+    api.position.set((state.mouse.x * state.viewport.width) / 2, -state.viewport.height / 2, 0);
+    api.rotation.set(0, 0, (state.mouse.x * Math.PI) / 5);
+  });
+  return (
+    <mesh ref={ref}>
+      <boxGeometry args={args} />
+      <meshStandardMaterial color="lightblue" />
+    </mesh>
+  )
+}
+
+function Enemy({ args = [2, 0.5, 1], color, ...props }) {
+  const [ref] = useBox(() => ({ args, ...props }))
+  return (
+    <mesh ref={ref}>
+      <boxGeometry args={args} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <div className="stars-component">
+        <div className="stars stars-1"></div>
+        <div className="stars stars-2"></div>
+        <div className="stars stars-3"></div>
+      </div>
+      <Canvas camera={{ position: [0, 5, 12], fov: 50 }}>
+        <ambientLight intensity={0.3} />
+        <pointLight position={[10, 10, 5]} />
+        <pointLight position={[-10, -10, -5]} />
+        <Physics gravity={[0, -30, 0]} defaultContactMaterial={{ restitution: 1.1 }}>
+          <Ball />
+          <Paddle />
+          <Enemy color="orannge" position={[2, 1, 0]} />
+          <Enemy color="lightgreen" position={[-5, -1, 0]} />
+          <Enemy color="red" position={[-8, 3, 0]} />
+          <Enemy color="blue" position={[7, 4, 0]} />
+          <Enemy color="hotpink" position={[-2, 3, 0]} />
+        </Physics>
+      </Canvas>
+    </>
+  )
+}
